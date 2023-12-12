d7f4f3d49e0be7bf71da118f45b31d267f43890b
OnyekwereMichael <onyekweremichael55@gmail.com>
2023-12-12 02:35:14 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Header from './PAGES/home';
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+import Scheme from './PAGES/Scheme';
+import Signup from './PAGES/Signup';
+import { AnimatePresence } from "framer-motion";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div>
+      <Router>
+  <Navbar />
+              <Routes>
+                <Route path = "/" element={<Header/>}/> 
+                <Route path = "/Scheme" element={<Scheme />}/> 
+                <Route path = "/Signup" element={<Signup />}/> 
+                <Route path = "*" element={"PAGE NOT FOUND"}/> 
+                </Routes>
+     <Footer />
+        </Router>
+       
+ </div>
+ 
   );
 }
 
